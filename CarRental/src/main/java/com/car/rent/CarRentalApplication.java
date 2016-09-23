240106f8aef1b6962d768effbaeadc2d8a088066
arungithub123 <arunnnet@gmail.com>
2016-09-23 16:54:30 -0700

    arun

--- CarRental/src/main/java/com/car/rent/CarRentalApplication.java
+++ CarRental/src/main/java/com/car/rent/CarRentalApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarRentalApplication {
-
+//how ar eyou?
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
 		//system.out.println();
