f0a381def48da413d7356516ef7453d8f81c4423
paulosgetu <paulos.getu@gmail.com>
2016-09-23 19:33:59 -0500

    paulos_test

--- CarRental/src/main/java/com/car/rent/CarRentalApplication.java
+++ CarRental/src/main/java/com/car/rent/CarRentalApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CarRentalApplication {
-//how ar eyou?
 	public static void main(String[] args) {
 		SpringApplication.run(CarRentalApplication.class, args);
 		
