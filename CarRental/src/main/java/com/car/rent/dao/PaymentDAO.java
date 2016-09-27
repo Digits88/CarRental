e33b27a4672b17b4dea077acfe6de83a1c6f1433
arungithub123 <arunnnet@gmail.com>
2016-09-26 20:15:48 -0700

    Arun Payment Committed

--- CarRental/src/main/java/com/car/rent/dao/PaymentDAO.java
+++
@@ -1,9 +0,0 @@
-package com.car.rent.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.car.rent.domain.Payment;
-
-public interface PaymentDAO extends JpaRepository<Payment, Long> {
-
-}
