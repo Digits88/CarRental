82a1ea3e3e1b0b42ea05ca9c3c374e151fdd7cf0
paulosgetu <paulos.getu@gmail.com>
2016-09-27 20:13:17 -0500

    12

--- CarRental/src/main/java/com/car/rent/vehicle/controller/VehicleController.java
+++ CarRental/src/main/java/com/car/rent/vehicle/controller/VehicleController.java
@@ -6,7 +6,6 @@ import java.util.List;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Required;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,9 +13,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.car.rent.domain.Vehicle;
